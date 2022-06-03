d740fc0d5f3d29da85f8ab154874af51c5ccb11b
hamza nassour <hamza.nassour@edu.uca.ma>
2022-06-03 15:02:47 -0700

    linking find endpoint with result page

--- src/main/java/com/example/semsar3/Controllers/LogementController.java
+++ src/main/java/com/example/semsar3/Controllers/LogementController.java
@@ -2,10 +2,13 @@ package com.example.semsar3.Controllers;
 
 import com.example.semsar3.entities.Logement;
 import com.example.semsar3.repositories.LogementRepository;
+import com.example.semsar3.repositories.TypeRepository;
+import com.example.semsar3.repositories.VilleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,21 +16,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
-@RequestMapping("/logement")
 public class LogementController {
 
     @Autowired
     LogementRepository logementRepository ;
-    @ResponseBody
+
+    @Autowired
+    VilleRepository villeRepository;
+    @Autowired
+    TypeRepository typeRepository;
+
     @RequestMapping("/find")
-    public List<Logement> findLogement( String ville , String type){
-
-        return  logementRepository.findLogementsByVilleNomAndTypeTypeLog(ville , type );
-    }
-
-    @RequestMapping("/add")
-    public void addLogement( ) {
-
+    public String  findLogement(Model model , String ville , String type){
+        model.addAttribute("villes" ,villeRepository.findAll() );
+        model.addAttribute("types" , typeRepository.findAll() );
+        List<Logement> logements = logementRepository.findLogementsByVilleNomAndTypeTypeLog(ville , type);
+        model.addAttribute("logements" , logements);
+        return  "resultats";
 
     }
 
