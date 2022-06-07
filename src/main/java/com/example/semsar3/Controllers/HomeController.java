2239250ff60688801f83fdcde215a2ce3f3bb13a
hamza nassour <hamza.nassour@edu.uca.ma>
2022-06-07 07:09:03 -0700

    fixing an issue with the home page

--- src/main/java/com/example/semsar3/Controllers/HomeController.java
+++ src/main/java/com/example/semsar3/Controllers/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -18,9 +19,13 @@ public class HomeController {
     TypeRepository typeRepository;
 
     @RequestMapping("/index")
-    public  String index(Model model ){
+    public  String index(Model model ,
+                         @RequestParam(value = "ville" , defaultValue = "Choisiser") String ville ,
+                         @RequestParam(value = "type" , defaultValue = "Choisiser") String type) {
         model.addAttribute("villes" ,villeRepository.findAll() );
         model.addAttribute("types" , typeRepository.findAll() );
+        model.addAttribute("ville" , ville);
+        model.addAttribute("type" , type);
         return "acceuil";
 
     }
